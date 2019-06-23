db0bc8f269c39da5812bf3c4e667de4cecd32463
samreen <samreensingh61@yahoo.in>
2019-06-23 23:44:44 +0530

    Updates

---
+++ precache-manifest.a313442e862cce1e0df65b7e57426aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039f59dec2b9783f1cd2fbb852ab9413",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "5ea698db9b7b2c837ed0",
+    "url": "/test1/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "c25b115bfd87db0a5a53",
+    "url": "/test1/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5ea698db9b7b2c837ed0",
+    "url": "/test1/static/js/2.3d8715f4.chunk.js"
+  },
+  {
+    "revision": "c25b115bfd87db0a5a53",
+    "url": "/test1/static/js/main.f3893aee.chunk.js"
+  },
+  {
+    "revision": "110df05cd91b572a9bfe",
+    "url": "/test1/static/js/runtime~main.044b3335.js"
+  }
+]);
